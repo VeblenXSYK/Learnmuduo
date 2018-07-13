cb2d10bcac09143f5e005f6025b4ede628c6c512
VeblenXSYK <1207316368@qq.com>
2018-07-13 18:30:19 +0800

    增加CTcpClient处理TCP粘包与分包的功能、增加CTcpClient使用protobuf与服务端交换数据的功能

--- vehicleweb/TcpClient.py
+++ vehicleweb/TcpClient.py
@@ -4,10 +4,23 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 import os, time
 import socket, select
+import struct
 import logging
 import threading
 import WWSGC
+import commweb_pb2
 		
+		
+"""
+	struct TransportFormat
+	{
+		int32_t  headLen;
+		int32_t  nameLen;
+		char     typeName[nameLen];
+		char     protobufData[headLen-nameLen-8];
+		int32_t  checkSum;
+	}
+"""
 
 class CTcpClient(threading.Thread):
 
@@ -18,6 +31,41 @@ class CTcpClient(threading.Thread):
 		self.__clientsock = None
 		self.__delaytime = 2
 		self.__run = False
+		
+	# 填充网络传输信息
+	def FillTransferInfo(self, proto, data):
+		
+		namelen = 0; namesize = 4 
+		messagename = proto.DESCRIPTOR.full_name; messagesize = len(messagename) + 1	#messagename以'\0'结尾
+		protosize = len(data)
+		checksum = 0; checksize = 4
+		
+		headlen = namesize + messagesize + protosize + checksize				
+		namelen = messagesize
+		
+		# 打包为TransportFormat
+		fmt = '!ii' + str(namelen) + 's' + str(protosize) + 'si'
+		packed = struct.pack(fmt, headlen, namelen, messagename, data, checksum)
+		
+		return packed
+		
+	def SendData(self, data):
+	
+		if self.__clientsock != None:
+			self.__clientsock.sendall(data)
+			
+	def TestTcpConn(self):
+	
+		# 序列化protobuf
+		proto = commweb_pb2.CmdInfo()
+		proto.type = ord('@')
+		data = proto.SerializeToString()
+
+		# 填充TransportFormat
+		packed = self.FillTransferInfo(proto, data)
+		
+		# 发送出去
+		self.SendData(packed)
 		
 	def Connect(self):
 	
@@ -50,18 +98,56 @@ class CTcpClient(threading.Thread):
 	def RecvMessage(self):
 		
 		self.__run = True
+		recvbuffer = bytes()		# 接收缓存区，用于处理TCP粘包与分包问题
+		headsize = 4				# 头部占用的空间大小
+		minbodylen = 4*2+2			# nameLen + typeName + checkSum
+		maxmessageLen = 1*1024*1024	# 1M
 
 		try:
 
 			inputs = [self.__clientsock]
 			
 			while self.__run:
+			
 				readable, writeable, exceptional = select.select(inputs, [], [])
 				for fd in readable:
 					if fd == self.__clientsock:
-						datahead = self.__clientsock.recv(WWSGC.DATALEN_HEAD)
-						if len(datahead) != 0:
-							pass
+						data = self.__clientsock.recv(1024)
+						
+						if len(data) != 0:
+						
+							# 把数据存入缓冲区
+							recvbuffer += data
+							
+							while True:
+							
+								# 接收的数据量少于门限值
+								if len(recvbuffer) < minbodylen:
+									logging.info('datapacket size less then minmessageLen')
+									break
+									
+								# 获取头部长度的值与消息名长度的值
+								bodylen, messagenamelen = struct.unpack('!ii', recvbuffer[:headsize*2])
+								
+								# 分包处理
+								if len(recvbuffer) < bodylen + headsize:
+									logging.info('datapacket size less then bodylen')
+									break
+									
+								# 读取消息正文内容
+								protolen = bodylen - messagenamelen - headsize * 2
+								fmt = '!' + str(messagenamelen) + 's' + str(protolen) + 'si'
+								messagename, protodata, checksum = struct.unpack(fmt, recvbuffer[headsize*2:headsize+bodylen])
+								
+								# 根据消息名进行相应的处理
+								if messagename[:-1] == 'commweb.VehicleInfo':
+									vi = commweb_pb2.VehicleInfo()
+									vi.ParseFromString(protodata)
+									print vi.fW, vi.fV
+									print vi.aInfo[0].fW, vi.aInfo[0].fV
+								
+								# 粘包处理
+								recvbuffer = recvbuffer[headsize+bodylen:]
 						else:
 							logging.info('Remote TcpServer Close')
 							self.__run = False
@@ -90,6 +176,9 @@ class CTcpClient(threading.Thread):
 			if self.Connect() == False:
 				while self.Retry() == False:
 					pass
+					
+			# 测试TCP通信
+			self.TestTcpConn()
 		
 			# 接收信息
 			self.RecvMessage()
