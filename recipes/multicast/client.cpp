dadeed41240fcd0fb7206be7f03910891308fbad
VeblenXSYK <1207316368@qq.com>
2020-07-12 15:40:33 +0800

    修改《linux下组播实现》例程

--- recipes/multicast/client.cpp
+++ recipes/multicast/client.cpp
@@ -4,37 +4,38 @@
 #include <arpa/inet.h>
 #include <net/if.h>
 
-#define SERVER_PORT 	8000
-#define CLIENT_PORT 	9000
-#define GROUP_ADDR 		"239.0.0.2"
+#define GROUP_PORT 	9000
+#define GROUP_ADDR 	"239.0.0.2"
 
 int main(int argc, char *argv[])
 {   
     int confd = socket(AF_INET, SOCK_DGRAM, 0);
 	
-	struct sockaddr_in localaddr;
-    bzero(&localaddr, sizeof(localaddr));                                   /* 初始化*/
-    localaddr.sin_family = AF_INET;
-    inet_pton(AF_INET, "0.0.0.0" , &localaddr.sin_addr.s_addr);
-    localaddr.sin_port = htons(CLIENT_PORT);
+	struct sockaddr_in recvaddr;
+    bzero(&recvaddr, sizeof(recvaddr));                                   	/* 初始化接收地址 */
+    recvaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, GROUP_ADDR, &recvaddr.sin_addr.s_addr);				/* 接收地址必须是组播地址+端口 */
+    recvaddr.sin_port = htons(GROUP_PORT);
 
-    bind(confd, (struct sockaddr *)&localaddr, sizeof(localaddr));
+    bind(confd, (struct sockaddr *)&recvaddr, sizeof(recvaddr));
 
 	struct ip_mreqn group;                                                  /* 组播结构体 */
     inet_pton(AF_INET, GROUP_ADDR, &group.imr_multiaddr);                   /* 设置组播组地址 */
-    inet_pton(AF_INET, "0.0.0.0", &group.imr_address);                      /* 使用本地任意IP添加到组播组 */
-    group.imr_ifindex = if_nametoindex("ens33");                             /* 设置网卡名 编号 ip ad */
-	printf("%d\n", group.imr_ifindex);
+	inet_pton(AF_INET, "192.168.5.108", &group.imr_address);                /* 设置本地地址添加到组播组 */
+    group.imr_ifindex = if_nametoindex("ens33");                            /* 设置本地地址的设备编号 ip a */
 	
-    setsockopt(confd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &group, sizeof(group));/* 将client加入组播组(注意：不需要服务器加入多播组) */
+    setsockopt(confd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &group, sizeof(group));/* 加入组播组(注意：不需要服务器加入多播组) */
 	
 	char buf[1024];
-    while (1) 
+	int count = 5;
+    while (--count) 
 	{
 		memset(buf, 0, sizeof(buf));
         ssize_t len = recvfrom(confd, buf, sizeof(buf), 0, NULL, 0);
         write(STDOUT_FILENO, buf, len);
     }
+	
+	setsockopt(confd, IPPROTO_IP, IP_DROP_MEMBERSHIP, &group, sizeof(group));/* 退出组播组 */
     close(confd);
     return 0;
 }
--- recipes/multicast/server.cpp
+++ recipes/multicast/server.cpp
@@ -1,49 +1,45 @@
+/*
+	多播服务器的程序设计，不需要服务器加入多播组，可以直接向某个多播组发送数据
+*/
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <net/if.h>
 
-#define SERVER_PORT 	8000
-#define CLIENT_PORT 	9000
-#define GROUP_ADDR 		"239.0.0.2"
-//239.0.0.0～239.255.255.255
+#define MCAST_PORT 9000
+#define MCAST_ADDR "239.0.0.2"				// 一个局部连接多播地址，路由器不进行转发
+#define MCAST_DATA "BROADCAST TEST DATA\n"	// 多播发送的数据
+#define MCAST_INTERVAL 2					// 发送间隔时间
 
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);            /* 构造用于UDP通信的套接字 */
-    
-	struct sockaddr_in serveraddr;
-    bzero(&serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;                        /* IPv4 */
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);         /*本地任意IP INADDR_ANY = 0 */
-    serveraddr.sin_port = htons(SERVER_PORT);
-
-    bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-
-	struct ip_mreqn group;
-    inet_pton(AF_INET, GROUP_ADDR, &group.imr_multiaddr);   /* 设置组播组的地址 */
-    inet_pton(AF_INET, "0.0.0.0", &group.imr_address);      /* 本地任意IP自动分配有效IP */
-    group.imr_ifindex = if_nametoindex("ens33");             /* 给出网卡名，转换为对应编号：eth0 --> 编号         ，，  命令:ip ad */
-
-    setsockopt(sockfd, IPPROTO_IP, IP_MULTICAST_IF, &group, sizeof(group));  /*获取组播权限*/
-
-	struct sockaddr_in clientaddr;
-    bzero(&clientaddr, sizeof(clientaddr));                 /* 构造client地址 IP+端口号 */
-    clientaddr.sin_family = AF_INET;
-    inet_pton(AF_INET, GROUP_ADDR, &clientaddr.sin_addr.s_addr); /* IPv4  239.0.0.2+9000 */
-    clientaddr.sin_port = htons(CLIENT_PORT);
-
-	int i = 0;
-	char buf[1024];
-    while (1) 
+	int s = socket(AF_INET, SOCK_DGRAM, 0);
+	if (s == -1)
 	{
-		memset(buf, 0, sizeof(buf));
-        sprintf(buf, "multicast %d\n", i++);
-        sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&clientaddr, sizeof(clientaddr));
-        sleep(1);
-    }
-    close(sockfd);
-    return 0;
+		perror("socket()");
+		return -1;
+	}
+
+	struct sockaddr_in mcast_addr;
+	memset(&mcast_addr, 0, sizeof(mcast_addr));
+	mcast_addr.sin_family = AF_INET;
+	mcast_addr.sin_addr.s_addr = inet_addr(MCAST_ADDR);
+	mcast_addr.sin_port = htons(MCAST_PORT);
+
+	while(1) 
+	{
+		// 向多播地址发送数据
+		int n = sendto(s, MCAST_DATA, sizeof(MCAST_DATA), 0, (struct sockaddr*)&mcast_addr, sizeof(mcast_addr));
+		if( n < 0)
+		{
+			perror("sendto()");
+			return -2;
+		}
+		sleep(MCAST_INTERVAL);
+	}
+	return 0;
 }
